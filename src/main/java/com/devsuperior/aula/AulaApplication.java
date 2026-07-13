3d248670b4c3f78e1b78b70732a7a4f286a5cb08
tiago-costa-s <tiagocostasantos0013@gmail.com>
2026-07-13 19:26:58 -0300

    Save PersonDTO

--- src/main/java/com/devsuperior/aula/AulaApplication.java
+++ src/main/java/com/devsuperior/aula/AulaApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class gitAulaApplication {
+public class AulaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AulaApplication.class, args);
