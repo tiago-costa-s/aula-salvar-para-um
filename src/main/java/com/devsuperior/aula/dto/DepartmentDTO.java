2d3907c60fcde966fe43fd16cf5b5d7857022625
tiago-costa-s <tiagocostasantos0013@gmail.com>
2026-07-13 18:25:37 -0300

    feat - save PersonDepartmentDTO

---
+++ src/main/java/com/devsuperior/aula/dto/DepartmentDTO.java
@@ -0,0 +1,36 @@
+package com.devsuperior.aula.dto;
+
+import com.devsuperior.aula.entities.Department;
+
+public class DepartmentDTO {
+    private Long id;
+    private String name;
+
+    public DepartmentDTO() {    }
+
+    public DepartmentDTO(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public DepartmentDTO(Department entity) {
+        id = entity.getId();
+        name = entity.getName();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
